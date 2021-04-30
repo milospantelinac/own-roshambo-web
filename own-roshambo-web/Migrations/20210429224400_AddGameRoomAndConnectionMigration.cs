a7fb18e01d2d1855c9067839bb4e3aee4926efd1
pantelinac <milos.pantelinac@madduckcode.com>
2021-04-30 22:21:58 +0200

    setup user and add migrations for GameRoom and Connection

---
+++ own-roshambo-web/Migrations/20210429224400_AddGameRoomAndConnectionMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OwnRoshamboWeb.Migrations
+{
+    public partial class AddGameRoomAndConnectionMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
